3fa465f67daf65ac818ad02d4abe8596300cb45f
Rymedero <rymedero.21@est.ucab.edu.ve>
2026-02-19 12:30:15 -0400

    feat: Update TrainingSchedule component for improved layout and styling

--- src/pages/InfoReto/components/TabsComponents/TrainingSchedule.tsx
+++ src/pages/InfoReto/components/TabsComponents/TrainingSchedule.tsx
@@ -18,51 +18,100 @@ export function TrainingSchedule() {
                     <thead>
                     <tr>
                         
-                        <th className="text-primary">Evento</th>
-                        <th className="text-primary">Fecha</th>
-                        <th className="text-primary">Hora</th>
+                        <th className="text-primary text-sm">Evento</th>
+                        <th className="text-primary text-sm">Fecha</th>
+                        <th className="text-primary text-sm">Hora</th>
+                        <th className="text-primary text-sm">Modalidad</th>
                     </tr>
                     </thead>
                     <tbody>
                 
                     <tr>
-                        <th className="text-gray-400">Taller de formación 1</th>
+                        <th className="text-white">Taller de formación 1</th>
                         <td className="text-gray-400">Martes 24 de febrero</td>
                         <td className="text-gray-400">8:00 am – 1:00 pm</td>
+                        <td className="text-gray-400">Híbrido</td>
+                    </tr>
+
+                    <tr>
+                        <th className="text-gray-400">Práctica adicional #1 – Grupo 1</th>
+                        <td className="text-gray-400">Viernes 27 de febrero</td>
+                        <td className="text-gray-400">3:00 pm – 5:00 pm</td>
+                        <td className="text-gray-400">Virtual</td>
+                    </tr>
+
+                    <tr>
+                        <th className="text-gray-400">Práctica adicional #1 – Grupo 2</th>
+                        <td className="text-gray-400">Viernes 06 de marzo</td>
+                        <td className="text-gray-400">3:00 pm – 5:00 pm</td>
+                        <td className="text-gray-400">Virtual</td>
                     </tr>
                 
                     <tr>
-                        <th className="text-gray-400">Taller de formación 2</th>
+                        <th className="text-white">Taller de formación 2</th>
                         <td className="text-gray-400">Martes 10 de marzo</td>
                         <td className="text-gray-400">8:00 am – 1:00 pm</td>
-                        
+                        <td className="text-gray-400">Híbrido</td>
+                    </tr>
+
+                    <tr>
+                        <th className="text-gray-400">Práctica adicional #2 – Grupo 1</th>
+                        <td className="text-gray-400">Viernes 13 de marzo</td>
+                        <td className="text-gray-400">3:00 pm – 5:00 pm</td>
+                        <td className="text-gray-400">Virtual</td>
+                    </tr>
+
+                    <tr>
+                        <th className="text-gray-400">Práctica adicional #2 – Grupo 2</th>
+                        <td className="text-gray-400">Viernes 20 de marzo</td>
+                        <td className="text-gray-400">3:00 pm – 5:00 pm</td>
+                        <td className="text-gray-400">Virtual</td>
+                    </tr>
+
+                     <tr>
+                        <th className="text-white">Demo Day</th>
+                        <td className="text-gray-400">Lunes 23 de marzo</td>
+                        <td className="text-gray-400">8:00 am – 1:00 pm</td>
+                        <td className="text-gray-400">Presencial</td>
                     </tr>
                     
                     <tr>
-                        <th className="text-gray-400">Taller de formación 3</th>
+                        <th className="text-white">Taller de formación 3</th>
                         <td className="text-gray-400">Martes 28 de abril</td>
                         <td className="text-gray-400">8:00 am – 1:00 pm</td>
+                        <td className="text-gray-400">Híbrido</td>
                         
                     </tr>
 
+
+
                     <tr>
-                        <th className="text-gray-400">Demo Day</th>
-                        <td className="text-gray-400">Lunes 23 de marzo</td>
-                        <td className="text-gray-400">8:00 am – 1:00 pm</td>
-                        
+                        <th className="text-gray-400">Práctica adicional #3 – Grupo 1</th>
+                        <td className="text-gray-400">Viernes 08 de mayo</td>
+                        <td className="text-gray-400">3:00 pm – 5:00 pm</td>
+                        <td className="text-gray-400">Virtual</td>
                     </tr>
 
                     <tr>
-                        <th className="text-gray-400">Final</th>
+                        <th className="text-gray-400">Práctica adicional #3 – Grupo 2</th>
+                        <td className="text-gray-400">Viernes 15 de mayo</td>
+                        <td className="text-gray-400">3:00 pm – 5:00 pm</td>
+                        <td className="text-gray-400">Virtual</td>
+                    </tr>
+
+
+                    <tr>
+                        <th className="text-white">Final</th>
                         <td className="text-gray-400">Viernes 29 de mayo</td>
                         <td className="text-gray-400">8:00 am – 1:00 pm</td>
+                        <td className="text-gray-400">Presencial</td>
                     </tr>
                     </tbody>
                 </table>
 
                 <div className="mt-4 justify">
                     <p className="text-gray-600 dark:text-gray-400 mb-4 justify">
-                       El requisito de asistencia obligatoria (3 sesiones formativas y 2 de mentoría) garantiza que todos los participantes adquieran esta base de conocimiento fundamental, siendo la condición indispensable para asegurar su preparación y avanzar hacia el evento final.
+                       El requisito de asistencia obligatoria (3 sesiones formativas y 2 de mentoría) garantiza que todos los participantes adquieran esta base de conocimiento fundamental, siendo la condición indispensable para asegurar su preparación y avanzar hacia el evento final. Las prácticas adicionales son de carácter opcional, diseñadas para brindar apoyo adicional a los estudiantes que deseen profundizar en los temas tratados en las formaciones o necesiten orientación personalizada para el desarrollo de sus proyectos.
                     </p>
                 </div>
 
