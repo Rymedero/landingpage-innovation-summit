032918669fbddbd44b5cc09eab7779e2555b74ff
Douglas Rojas <Douglas.Rojas@ve.ey.com>
2026-03-22 21:50:38 -0400

    fix: Remove unnecessary query parameters from YouTube video ID for SALA FUSION

--- src/pages/InnovationSummit2026/pages/Salas/Salas.tsx
+++ src/pages/InnovationSummit2026/pages/Salas/Salas.tsx
@@ -8,7 +8,7 @@ import { FooterSummit } from "../../components/Footer";
 // const VITE_ID_VIDEO_3 = import.meta.env.VITE_ID_VIDEO_3;
 
 const VITE_ID_VIDEO_1 = "___7kD-uLkQ";
-const VITE_ID_VIDEO_2 = "riwNcB8C8nQ&pp=ygUCZXk%3D";
+const VITE_ID_VIDEO_2 = "riwNcB8C8nQ";
 const VITE_ID_VIDEO_3 = "EWl12PcdK2w";
 
 const YOUTUBE_LIVES = [
